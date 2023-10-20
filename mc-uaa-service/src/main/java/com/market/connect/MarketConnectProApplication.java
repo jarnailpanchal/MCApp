3f6660102cfa04633361e9d8f9e8add2ce87b8b0
jarnailpanchal <jarnailpanchal31@gmail.com>
2023-10-20 14:20:06 +0530

    initial commit

--- mc-uaa-service/src/main/java/com/market/connect/MarketConnectProApplication.java
+++ mc-uaa-service/src/main/java/com/market/connect/MarketConnectProApplication.java
@@ -1,8 +1,10 @@
 package com.market.connect;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class MarketConnectProApplication {
 
